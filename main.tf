11dfeb80bc43c16b96971bd8c26b5d4b1b79fe86
nandini965 <nandhini965@gmail.com>
2024-01-18 22:45:06 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-  tags = merge(var.tags, { Name : "${env}-${component}-sg" })
+  tags = merge(var.tags, { Name : "${name}-${env}-sg" })
 }
 
   resource "aws_launch_template" "template" {
